8ab5b7ba14c3ddd263484242f6f95d118ace2c2c
CaioAssis <caio.ab.assis@gmail.com>
2024-08-14 21:51:22 -0300

    cria primeiros componentes

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StyledInput from '../component/styled-input';
+import StyledButton from '../component/styled-button';
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
+      <StyledInput
+      placeholder='Digite seu login'
+      onChangeText={(texto)=> console.log(texto)}
+      />
+      <StyledInput
+      placeholder='Digite sua senha'
+      onChangeText={(texto)=> console.log(texto)}
+      />
+      <StyledButton 
+      text='Login'
+      color='green'
+      onClick={()=> console.log('batata')}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
