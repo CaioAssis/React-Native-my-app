ccce8193c2276803970ff804c034de135c668d93
CaioAssis <caio.ab.assis@gmail.com>
2024-09-11 20:54:20 -0300

    aula 09 11

--- app/home.tsx
+++ app/home.tsx
@@ -1,33 +1,21 @@
-import { Link } from 'expo-router';
-import { Text, View } from 'react-native';
-import Ex1Box from '../components/ex1-box';
+import { View, StyleSheet } from "react-native";
+import {Text} from "react-native-paper"
+
 export default function Home() {
     return (
-        <View style={{ flex: 1, justifyContent: 'space-between' }}>
-            <View style={{ alignSelf: 'flex-start' }}>
-                <Text
-                    style={{ textAlign: 'center', marginBottom: 20 }}
-                >Login com sucesso!
-                </Text>
-                <Ex1Box
-                    image={require('../assets/booster2.png')}
-                    text='Isso é um teste. Isso é um teste. Isso é um teste. Isso é um teste. Isso é um teste. Isso é um teste. Isso é um teste. Isso é um teste. Isso é um teste. Isso é um teste. Isso é um teste. Isso é um teste. Isso é um teste. Isso é um teste. Isso é um teste. Isso é um teste. '
-                />
-                <Ex1Box
-                    image={require('../assets/pepelogoo.png')}
-                    text='Teste 2. Teste 2. Teste 2. Teste 2. Teste 2. Teste 2. Teste 2. Teste 2. Teste 2. Teste 2. Teste 2. Teste 2. Teste 2. '
-                />
-                <Ex1Box
-                    image={require('../assets/icon.png')}
-                    text='Teste 3.'
-                />
-            </View>
-            <View style={{ justifyContent: 'flex-end' }}>
-                <Link href='/'
-                    style={{ textAlign: 'center', fontSize: 20, backgroundColor: 'green', color: 'white', borderRadius: 10 }}>
-                    Voltar para Login
-                </Link>
-            </View>
+        <View style={styles.container}>
+
+        <Text>Olá usuário!</Text>
         </View>
-    );
+    )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+        padding: 20,
+    },
+})
---
+++ context/auth.tsx
@@ -0,0 +1,45 @@
+import { router } from "expo-router"
+import { ReactNode, createContext, useContext, useState } from "react"
+
+interface IUserLogin {
+    email: string
+    password: string
+}
+
+interface IAuthContext {
+    user: IUserLogin
+    setUser: (user: IUserLogin) => void
+    handleLogin: () => void
+}
+
+interface IAuthProviderProps {
+    children: ReactNode
+}
+
+const AuthContext = createContext<IAuthContext>({} as IAuthContext)
+
+//FC = function component
+export const AuthProvider: React.FC<IAuthProviderProps> = ({ children }) => {
+
+    const [user, setUser] = useState<IUserLogin>({email: '', password: ''})
+    const handleLogin = () => {
+        console.log(user)
+        if(user.email == 'admin' && user.password == 'admin'){
+            setUser(user)
+            router.push('home')
+        } else {
+            alert('email ou senha inválidos!')
+        }
+    }
+
+    return (
+        <AuthContext.Provider value={{user, setUser, handleLogin}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export function useAuth(){
+    const context = useContext(AuthContext)
+    return context
+}
